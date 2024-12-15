7cd1f04cafbd9b1c86585dcba3e4125e4f199549
Gulilil <juan.csantoso@gmail.com>
2024-12-16 02:59:05 +0700

    add python parser

--- process/json2clip.py
+++ process/json2clip.py
@@ -1,48 +1,69 @@
-import json
 import os
+import pandas as pd
 
-DATA_MEDS_DIR = os.path.join(os.getcwd(), "data_meds")
-data_path = os.path.join(DATA_MEDS_DIR, 'processed_data.json')
+DATA_MEDS_DIR = os.path.join(os.getcwd(), "data_meds_new")
+data_path = os.path.join(DATA_MEDS_DIR, 'data.csv')
 
 # Save item
-items_facts = []
-items_facts.append("(deftemplate data-item")
-items_facts.append("\t(slot compound)")
-items_facts.append("\t(slot compound_pair)")
-items_facts.append("\t(slot probability)")
-items_facts.append("\t(slot effect)")
-items_facts.append(")")
-
-# Write to a file
-with open("data-item.clp", "w") as file:
-    file.write("\n".join(items_facts))
-
-
-# Load JSON data
-with open(data_path, "r") as file:
-    data = json.load(file)
-
-# Convert JSON data to CLIPS-compatible format
 clips_facts = []
-
-for item in data:
-    compound = item['compound'].lower()
-    
-    for content_data in item['content']:
-        compound_pair = content_data['compound'].lower()
-        effect = content_data['effect']
-        probability = content_data['probability']
-
-        clips_facts.append(f"""
-(defrule check_interaction_{compound.replace(" ", "_")}_{compound_pair.replace(" ", "_")}
-   ({compound})
-   ({compound_pair})
-   (test (<= (/ (random) 32767.0) {probability})) 
-   =>
-   (printout t "The interaction of {compound} and {compound_pair} has resulted: {effect}" crlf)
+clips_facts.append("""
+(deftemplate drug-item
+	(slot drug-name)
+	(slot action)
+	(slot side-effect)
 )
+
+(deftemplate user-input
+   (slot first-drug)
+   (slot second-drug)
+)
+
+(deftemplate action-exception
+    (slot action1)
+    (slot action2)
+)
+
+(deftemplate add-side-effect
+    (slot action1)
+    (slot action2)
+    (slot new-side-effect)
+)                                
 """)
 
+
+# Load CSV data
+df = pd.read_csv(data_path)
+
+def adjust_string_val(text: str) -> str:
+  text = text.replace("\u03b1", "Alpha-")
+  text = text.replace("\u03b2", "Beta-")
+  text = text.replace("\u03bc", "Mu-")
+  splitted_text = text.split("•")
+  splitted_text = [sub_text.strip() for sub_text in splitted_text] # strip white space for all the element in list
+
+  if (len(splitted_text) == 1):
+     return splitted_text[0]
+  elif (len(splitted_text) == 2):
+     return f"{splitted_text[0]} and {splitted_text[1]}"
+  else:
+     result = ""
+     for i in range(len(splitted_text)):
+        if (i == len(splitted_text)-1):
+          result += f"and {splitted_text[i]}"
+        else:
+          result += f"{splitted_text[i]}, "
+
+clips_facts.append("(deffacts initial-facts")
+# Convert CSV data to CLIPS-compatible format
+for index, row in df.iterrows():
+    if (not pd.isna(row['Effects']) and row['Effects'] is not None): # Adjust this handling if needed        
+      name = row['Nama Obat'].title()
+      action = adjust_string_val(row['Mechanism of Action'])
+      effect = adjust_string_val(row['Effects'])
+      clips_facts.append(f"\t(drug-item (drug-name {name}) (action \"{action}\") (side-effect \"{effect}\"))")
+clips_facts.append(")")
+    
+
 # Write to a file
-with open("data-rule.clp", "w") as file:
+with open("data-drug.clp", "w") as file:
     file.write("\n".join(clips_facts))
